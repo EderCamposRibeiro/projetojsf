25c4432ea0c3181dc4cde5568aa331f0aa295606
teste <edercribeiro@gmail.com>
2020-07-10 09:23:43 -0300

    Projeto Criado - Merge e Update com JPA e JSF

---
+++ src/main/java/br/com/projetojsf/PessoaBean.java
@@ -0,0 +1,49 @@
+package br.com.projetojsf;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.dao.DaoGeneric;
+import br.com.entidades.Pessoa;
+
+@ViewScoped
+@ManagedBean
+public class PessoaBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	private Pessoa pessoa = new Pessoa();
+	private DaoGeneric<Pessoa> daoGeneric = new DaoGeneric<Pessoa>();
+	
+	public String salvar() {
+		pessoa = daoGeneric.merge(pessoa);
+		
+		return "";
+	}
+	
+	public String novo() {
+		pessoa = new Pessoa();
+		return "";
+	}
+	
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public DaoGeneric<Pessoa> getDaoGeneric() {
+		return daoGeneric;
+	}
+
+	public void setDaoGeneric(DaoGeneric<Pessoa> daoGeneric) {
+		this.daoGeneric = daoGeneric;
+	}
+	
+	
+}
