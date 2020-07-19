82738664c2c08acbaaf268868e5eb421718ab5fd
teste <edercribeiro@gmail.com>
2020-07-19 09:05:23 -0300

    Consumindo o RESTful do CEP - Web Service Cep - Parte 2

--- src/main/java/br/com/projetojsf/PessoaBean.java
+++ src/main/java/br/com/projetojsf/PessoaBean.java
@@ -1,6 +1,11 @@
 package br.com.projetojsf;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -69,7 +74,27 @@ public class PessoaBean implements Serializable{
 	}
 	
 	public void pesquisaCep(AjaxBehaviorEvent event) {
-		System.out.println("Método pesquisa cep chamado CEP: " + pessoa.getCep());
+		try {
+			URL url = new URL("https://viacep.com.br/ws/" + pessoa.getCep() +"/json/");
+			URLConnection connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			
+			String cep = "";
+			StringBuilder jsonCep = new StringBuilder();
+			
+			while ((cep = br.readLine()) != null) {
+				jsonCep.append(cep);
+			}
+			
+			System.out.println(jsonCep);
+			
+			br.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			mostrarMsg("Erro ao consultar o cep");
+		}
 	}
 	
 
