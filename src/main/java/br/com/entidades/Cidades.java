431cdcce964fcf665d30182e71a650c68fa4e43d
teste <edercribeiro@gmail.com>
2020-07-23 19:50:55 -0300

    Combos dependentes - Criando as entidades estado e cidades - Parte 1 e 2

---
+++ src/main/java/br/com/entidades/Cidades.java
@@ -0,0 +1,82 @@
+package br.com.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+public class Cidades implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+	private Estados estados_id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estados getEstados_id() {
+		return estados_id;
+	}
+
+	public void setEstados_id(Estados estados_id) {
+		this.estados_id = estados_id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidades other = (Cidades) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cidades [id=" + id + ", nome=" + nome + ", estados_id=" + estados_id + "]";
+	}
+	
+	
+	
+
+}
